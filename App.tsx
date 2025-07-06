90d57713a49106a0cc29f5fa9a8b20cc16a6fd6d
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2025-07-06 03:22:25 -0300

    Adiciona a config da statusBar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import { StatusBar } from "expo-status-bar"
 import Home from "./src/pages/Home/Home"
 
 export default function App() {
-  return <Home />
+  return (
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
+  )
 }
